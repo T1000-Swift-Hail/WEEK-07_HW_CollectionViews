c0ac19c284344365040a2c7349e87b5777da4f7e
Mastorhalzamil <mastorh.dogaim.has.s@tuwaiq.edu.sa>
2021-11-16 17:27:44 +0300

    donehw

---
+++ collectionView/collectionView/CollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionCell.swift
+//  collectionView
+//
+//  Created by Mastorah on 16/11/2021.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+    
+}
---
+++ collectionView/collectionView/secound screen.swift
@@ -0,0 +1,24 @@
+//
+//  secound screen.swift
+//  collectionView
+//
+//  Created by Mastorah on 16/11/2021.
+//
+
+import UIKit
+
+class secound_screen: UIViewController {
+
+    @IBOutlet weak var natureimg: UIImageView!
+    var secondimg: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    natureimg.image = secondimg
+
+
+
+}
