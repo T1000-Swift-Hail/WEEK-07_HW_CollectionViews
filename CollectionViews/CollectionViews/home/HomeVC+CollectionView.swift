20800d106bbba3537cef017755cfd0f3e2ce2c07
mohammed abdullah <Mohammed.Alhethely.has.s@tuwaiq.edu.sa>
2021-11-17 10:40:18 +0300

    Update colliction view

--- CollectionViews/CollectionViews/home/HomeVC+CollectionView.swift
+++ CollectionViews/CollectionViews/home/HomeVC+CollectionView.swift
@@ -43,7 +43,14 @@ extension HomeVC : UICollectionViewDataSource, UICollectionViewDelegate, UIColle
 
 }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (view.frame.width) / 2, height: (view.frame.height) / 6)
+        return CGSize(width: (view.frame.width) / 2, height: (view.frame.height) / 3)
     }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
     
 }
