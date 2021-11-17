b37e6042c71e3fbf4e78d501d8754b91112837b7
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-11-17 10:25:00 +0300

    complitHW with bouns

--- A gallery of Photos/A gallery of Photos/A gallery of Photos/Home/HomeVC+CollectionView.swift
+++ A gallery of Photos/A gallery of Photos/A gallery of Photos/Home/HomeVC+CollectionView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
 extension HomeVC:UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-     
+    
     func setDelegate() {
         collectionV.dataSource = self
         collectionV.delegate = self
@@ -21,45 +21,48 @@ extension HomeVC:UICollectionViewDataSource, UICollectionViewDelegate, UICollect
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return procedures.ProcedureData.count
     }
-   
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell =
                 collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CollectionCell  else { return UICollectionViewCell()}
-      
         
-        cell.imageView.image=UIImage(named: procedures.ProcedureData[indexPath.row].rawValue)
+        
+        cell.imageView.image = UIImage(named: procedures.ProcedureData[indexPath.row].rawValue)
         
         return cell
     }
-        
-        
-    //func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
+    
     
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: (view.frame.width) - 12, height: (view.frame.height) / 3)
-    }
         
+        
+    }
+    
+    
+    func collectionView(_ collecctionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+        
+        print("selecting item")
+        
+        performSegue(withIdentifier: "Detals", sender: nil)
+    
+   
+    }
 
+    override func prepare(for segue:UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Detals" {
+           
+            if let selectedItem = collectionV.indexPathsForSelectedItems?[0] {
+               
+                let destination = segue.destination as! ViewController
+                destination.fullImegName = procedures.ProcedureData[selectedItem.row].rawValue
+            }
+       
+        }
+
+}
+    
     
 }
-
-
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
--- A gallery of Photos/A gallery of Photos/A gallery of Photos/Home/ViewController.swift
+++ A gallery of Photos/A gallery of Photos/A gallery of Photos/Home/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fullImeg: UIImageView!
+   
+    var fullImegName : String!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        fullImeg.image = UIImage(named:fullImegName)
+    
     }
 
 
